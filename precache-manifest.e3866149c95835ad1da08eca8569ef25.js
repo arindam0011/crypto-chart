386a5a09271d2c29548922ef8fe485eae80197bb
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-18 13:29:28 +0530

    Updates

---
+++ precache-manifest.e3866149c95835ad1da08eca8569ef25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e9e245a28a9d0b30b9ef4366107e91",
+    "url": "/crypto-chart/index.html"
+  },
+  {
+    "revision": "abbdba2e86d41c0cdb74",
+    "url": "/crypto-chart/static/css/main.8bca819f.chunk.css"
+  },
+  {
+    "revision": "f18e9280047d2c2f67b5",
+    "url": "/crypto-chart/static/js/2.afef58d2.chunk.js"
+  },
+  {
+    "revision": "abbdba2e86d41c0cdb74",
+    "url": "/crypto-chart/static/js/main.c4cf9832.chunk.js"
+  },
+  {
+    "revision": "b6d6258b63ca1b63f79d",
+    "url": "/crypto-chart/static/js/runtime~main.adac0721.js"
+  }
+]);
