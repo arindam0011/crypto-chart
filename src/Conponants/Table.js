c95c673473c4899be697226e62d646dce1effc3a
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-21 01:28:16 +0530

    changes made

--- src/Conponants/Table.js
+++ src/Conponants/Table.js
@@ -3,22 +3,24 @@ import '../App.css';
 const Table = ({ data }) => {
   return (
     <table id="Table">
-        <tbody>
+      <tbody>
         {data.map((item) => (
-            
+
           <tr className="row" key={item.id}>
-            <td className="logo_name">
-              <div className="logo"><img src={item.image} alt={item.name} /></div> 
-              <div className="name">{item.name}</div>
+            <td>
+              <div className="logo_name">
+                <div className="logo"><img src={item.image} alt={item.name} /></div>
+                <div className="name">{item.name}</div>
+              </div>
             </td>
             <td className="symbol">{item.symbol.toUpperCase()}</td>
             <td className="price">${item.current_price}</td>
             <td className="volume">${item.total_volume}</td>
-            <td className="percentage" style={item.ath_change_percentage>=0?{color:"green"}:{color:"red"}}>{item.ath_change_percentage.toFixed(2)}%</td>
+            <td className="percentage" style={item.ath_change_percentage >= 0 ? { color: "green" } : { color: "red" }}>{item.ath_change_percentage.toFixed(2)}%</td>
             <td className="market_cap">Mkt Cap: ${item.market_cap}</td>
           </tr>
         ))}
-        </tbody>
+      </tbody>
     </table>
   );
 };
